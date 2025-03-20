3daaea5ca3eb3020695b4f707b9c8d6259c3359b
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-03-20 23:00:34 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 const sendMessageButton = document.querySelector("#send-message");
+const fileInput = document.querySelector("#file-input")
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper")
+const fileCancelButton = document.querySelector("#file-cancel")
 
 const API_KEY = ""
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`
 
 const userData = {
-    message: null
+    message: null,
+    file: {
+        data: null,
+        mime_type: null
+    }
 }
 
 
@@ -24,7 +31,7 @@ const generateBotresponse = async (incomingMessageDiv) => {
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify({
             contents: [{
-                parts: [{ text: userData.message}]
+                parts: [{ text: userData.message}, ...(userData.file.data ? [{ inline_data: userData.file }] : [])]
             }]
         })
     }
@@ -36,10 +43,11 @@ const generateBotresponse = async (incomingMessageDiv) => {
         const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
         messageElement.innerText = apiResponseText   
     }catch(error){
-        console.log(error)
+        console.log(error);
         messageElement.innerText = error.message;
         messageElement.style.color = "#ff0000";
     }finally {
+        userData.file = {};
         incomingMessageDiv.classList.remove("thinking");
         chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
     }
@@ -48,9 +56,12 @@ const generateBotresponse = async (incomingMessageDiv) => {
 const handleOutgoingMessage = (e) => {
     e.preventDefault();
     userData.message = messageInput.value.trim();
-    messageInput.value = ""
+    messageInput.value = "";
+    fileUploadWrapper.classList.remove("file-uploaded");
 
-    const messageContent = `<div class="message-text">${userData.message}</div>`;
+    const messageContent = `<div class="message-text">${userData.message}</div>
+                            ${userData.file.data ? `<img src="data:${userData.file.mime_type};base64,
+                            ${userData.file.data}" class="attachment"/>` : ""}`;
     const outGoingMessageDiv = createMessageElement(messageContent, "user-message");
     outGoingMessageDiv.querySelector(".message-text").textContent = userData.message
     chatBody.appendChild(outGoingMessageDiv);
@@ -77,8 +88,49 @@ const handleOutgoingMessage = (e) => {
 messageInput.addEventListener("keydown", (e) => {
     const userMessage = e.target.value.trim();
     if(e.key === "Enter" && userMessage){
-        handleOutgoingMessage(e)
+        handleOutgoingMessage(e);
     }
 });
 
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if(!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        fileUploadWrapper.querySelector("img").src = e.target.result;
+        fileUploadWrapper.classList.add("file-uploaded");
+        const base64String = e.target.result.split(",")[1]
+        
+        userData.file = {
+            data: base64String,
+            mime_type: file.type
+        }
+        fileInput.value = "";
+    }
+    reader.readAsDataURL(file);
+});
+
+fileCancelButton.addEventListener("click", () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded");
+})
+
+const picker = new EmojiMart.Picker({
+    theme: "light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onClickOutside: (e) => {
+        if(e.target.id === "emoji-picker") {
+            document.body.classList.toggle("show-emoji-picker");
+        }
+        else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
+
+document.querySelector(".chat-form").append(picker);
+
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
+document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
